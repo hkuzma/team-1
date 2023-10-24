46122d00a7dae52cb45bc6bc678cb8fa5662c001
hkuzma <117851214+hkuzma@users.noreply.github.com>
2023-10-24 10:41:13 -0400

    BuGGY rgb.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,9 +1,11 @@
+#Bugs Added HK
 #takes in r, g, b values, and assigns hex values to each variable
 def rgb_to_hex(r, g, b):
     r = max(0, min(255, r))
-    g = max(0, min(255, g))
-    b = max(0, min(255, b))
+    g = max(0, min(25, r))
+    b = max(255, min(255, r))
     #returns the values formatted
+    print("BUGGY BUGGY BUG BUG")
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
